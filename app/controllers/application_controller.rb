2182e9373c16e1126165b5fdd521acd4d41f0384
Blake Balbirnie <blakebalbirnie@gmail.com>
2018-03-07 17:08:33 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class App < Sinatra::Base
         get '/' do
           erb :super_hero
         end
-     	 
+
         post '/teams' do
           @team_name = params[:team][:name]
           @team_motto = params[:team][:motto]
